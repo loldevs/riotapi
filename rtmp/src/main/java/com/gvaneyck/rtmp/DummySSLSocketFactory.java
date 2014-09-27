8f0f21f6dd88bd58f3384d7905cd30bbb490deb7
Boreeas <malte.schuetze@fgms.de>
2014-09-27 08:40:31 +0200

    Added support for ignoring SSL cert chain errors using gvaneyck's DummySSLSocketFactory. Fixes #28

---
+++ rtmp/src/main/java/com/gvaneyck/rtmp/DummySSLSocketFactory.java
@@ -0,0 +1,83 @@
+/*
+      Copyright (C) 2012-2014 Gabriel Van Eyck
+
+    This software is provided 'as-is', without any express or implied
+    warranty. In no event will the authors be held liable for any damages
+    arising from the use of this software.
+
+    Permission is granted to anyone to use this software for any purpose,
+    including commercial applications, and to alter it and redistribute it
+    freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+    2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+    3. This notice may not be removed or altered from any source distribution.
+
+    Gabriel Van Eyck vaneyckster@gmail.com
+ */
+package com.gvaneyck.rtmp;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author Gabriel Van Eyck
+ */
+public class DummySSLSocketFactory extends SSLSocketFactory {
+    private SSLSocketFactory factory;
+
+    public DummySSLSocketFactory() {
+        try {
+            SSLContext sslcontent = SSLContext.getInstance("TLS");
+            sslcontent.init(null, new TrustManager[]{new DummyTrustManager()}, new java.security.SecureRandom());
+            factory = sslcontent.getSocketFactory();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace(System.out);
+        } catch (KeyManagementException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    public static SocketFactory getDefault() {
+        return new DummySSLSocketFactory();
+    }
+
+    public Socket createSocket(Socket socket, String s, int i, boolean flag) throws IOException {
+        return factory.createSocket(socket, s, i, flag);
+    }
+
+    public Socket createSocket(InetAddress inaddr, int i, InetAddress inaddr2, int j) throws IOException {
+        return factory.createSocket(inaddr, i, inaddr2, j);
+    }
+
+    public Socket createSocket(InetAddress inaddr, int i) throws IOException {
+        return factory.createSocket(inaddr, i);
+    }
+
+    public Socket createSocket(String s, int i, InetAddress inaddr, int j) throws IOException {
+        return factory.createSocket(s, i, inaddr, j);
+    }
+
+    public Socket createSocket(String s, int i) throws IOException {
+        return factory.createSocket(s, i);
+    }
+
+    public String[] getDefaultCipherSuites() {
+        return factory.getSupportedCipherSuites();
+    }
+
+    public String[] getSupportedCipherSuites() {
+        return factory.getSupportedCipherSuites();
+    }
+}
